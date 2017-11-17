a30bdb18c6ad4778dfcd3ad10841d1cbee0115b8
paulvaldez <valdezpaul00@gmail.com>
2017-11-17 14:54:37 -0600

    Created a Diary Controller.

---
+++ src/main/java/com/codeup/nutritrackr/controllers/DiaryController.java
@@ -0,0 +1,13 @@
+package com.codeup.nutritrackr.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DiaryController {
+
+    @GetMapping("/diary")
+    public String showDiaryPage() {
+        return "users/diary";
+    }
+}
