25decaea3052ffafd6b152d5e76bcad6e21586c9
paulvaldez <valdezpaul00@gmail.com>
2017-11-17 14:15:44 -0600

    Added the html for the login view.

--- src/main/java/com/codeup/nutritrackr/controllers/UsersController.java
+++ src/main/java/com/codeup/nutritrackr/controllers/UsersController.java
@@ -2,6 +2,7 @@ package com.codeup.nutritrackr.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -16,4 +17,5 @@ public class UsersController {
     public String showLoginPage() {
         return "users/login";
     }
+
 }
