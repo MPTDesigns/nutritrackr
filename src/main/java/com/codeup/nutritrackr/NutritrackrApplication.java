9707d1d26014920b55c5e9ebb35784a53f380775
Travis Payne <thpaggie910@gmail.com>
2017-11-17 09:51:29 -0600

    Initial commit

---
+++ src/main/java/com/codeup/nutritrackr/NutritrackrApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.nutritrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritrackrApplication.class, args);
+	}
+}
---
+++ src/main/java/com/codeup/nutritrackr/controllers/TestController.java
@@ -0,0 +1,14 @@
+package com.codeup.nutritrackr.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    @GetMapping(path = "/")
+    @ResponseBody
+    public String getHomePage() {
+        return "Site currently under construction";
+    }
+}
