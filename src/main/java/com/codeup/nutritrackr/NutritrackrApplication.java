05ec1b3a656e3bcf64f12f69ef3a68a317bb4c62
Travis Payne <thpaggie910@gmail.com>
2017-11-19 20:37:13 -0600

    Setting up deployment

--- src/main/java/com/codeup/nutritrackr/NutritrackrApplication.java
+++ src/main/java/com/codeup/nutritrackr/NutritrackrApplication.java
@@ -3,15 +3,21 @@ package com.codeup.nutritrackr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @EntityScan(
 		basePackageClasses = {NutritrackrApplication.class, Jsr310JpaConverters.class}
 )
 @SpringBootApplication
-public class NutritrackrApplication {
+public class NutritrackrApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NutritrackrApplication.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NutritrackrApplication.class);
 	}
 }
