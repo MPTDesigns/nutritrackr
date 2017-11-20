a1b18a7c96468474627daf0c24c946980f6083b3
Travis Payne <thpaggie910@gmail.com>
2017-11-20 16:58:22 -0600

    Update Meals service to resolve circular constructor issue

--- src/main/java/com/codeup/nutritrackr/services/Meals.java
+++ src/main/java/com/codeup/nutritrackr/services/Meals.java
@@ -2,6 +2,7 @@ package com.codeup.nutritrackr.services;
 
 import com.codeup.nutritrackr.models.Meal;
 import com.codeup.nutritrackr.models.User;
+import com.codeup.nutritrackr.repositories.MealRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,14 +10,14 @@ import java.util.List;
 @Service
 public class Meals {
 
-    private final Meals mealDao;
+    private final MealRepository mealDao;
 
-    public Meals(Meals mealDao) {
+    public Meals(MealRepository mealDao) {
         this.mealDao = mealDao;
     }
 
     private List<Meal> findAllByUser(User user){
 
-        return mealDao.findAllByUser(user);
+        return mealDao.getAllByUser(user);
     }
 }
