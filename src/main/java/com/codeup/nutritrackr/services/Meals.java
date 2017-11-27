d10e962aee333345476c057b2d9cbf902e8b8c44
Travis Payne <thpaggie910@gmail.com>
2017-11-27 17:14:31 -0600

    Update the Meals service to provide new services created in the MealRepository

--- src/main/java/com/codeup/nutritrackr/services/Meals.java
+++ src/main/java/com/codeup/nutritrackr/services/Meals.java
@@ -1,11 +1,12 @@
 package com.codeup.nutritrackr.services;
 
-import com.codeup.nutritrackr.models.FoodDrink;
 import com.codeup.nutritrackr.models.Meal;
+import com.codeup.nutritrackr.models.MealType;
 import com.codeup.nutritrackr.models.User;
 import com.codeup.nutritrackr.repositories.MealRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -22,10 +23,15 @@ public class Meals {
         return mealDao.getAllByUser(user);
     }
 
-//    public Meal save(Meal meal) {
-//        meal.setId(items.size() + 1);
-//        items.add(meal);
-//        return meal;
-//    }
+    public Meal save(Meal meal) {
+        return mealDao.save(meal);
+    }
 
+    public Meal findOne(long id) {
+        return mealDao.findOne(id);
+    }
+
+    public Meal findMealForDayByType(User user, LocalDate mealDate, MealType mealType) {
+        return mealDao.findByUserAndMealDateAndMealType(user, mealDate, mealType);
+    }
 }
