e40458a7fe898d9184523bcd58f0824edc1ec066
Travis Payne <thpaggie910@gmail.com>
2017-11-27 17:13:02 -0600

    WIP: Update the Diary Controller to display food entries by meal type for the current day

--- src/main/java/com/codeup/nutritrackr/controllers/DiaryController.java
+++ src/main/java/com/codeup/nutritrackr/controllers/DiaryController.java
@@ -1,32 +1,51 @@
 package com.codeup.nutritrackr.controllers;
 
 import com.codeup.nutritrackr.models.Meal;
+import com.codeup.nutritrackr.models.MealType;
+import com.codeup.nutritrackr.services.FoodDrinks;
+import com.codeup.nutritrackr.services.Meals;
+import com.codeup.nutritrackr.services.Users;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
 
 @Controller
 public class DiaryController {
 
-    //private final Diaries diaries;
+    private final Users users;
+    private final Meals meals;
+    private final FoodDrinks foodDrinks;
 
-    //public DiaryController(Diaries diaries) {
-    //   this.diaries = diaries;
-    //}
+    public DiaryController(Users users, Meals meals, FoodDrinks foodDrinks) {
+        this.users = users;
+        this.meals = meals;
+        this.foodDrinks = foodDrinks;
+    }
 
     @GetMapping("/diary")
     public String showDiaryPage(Model model) {
-        model.addAttribute("meal", new Meal());
+//        Meal mealToSave = new Meal(users.findOne(1));
+//        mealToSave.setMealType(MealType.BREAKFAST);
+//        mealToSave.setMealDate(LocalDate.now());
+//        mealToSave.setFoodItems(new ArrayList<>());
+//        mealToSave.getFoodItems().add(foodDrinks.findOne(3));
+//        meals.save(mealToSave);
+        Meal breakfast = meals.findMealForDayByType(
+            users.findOne(1),
+            LocalDate.now(),
+            MealType.BREAKFAST
+        );
+        model.addAttribute("breakfastItems", breakfast.getFoodItems());
+
         return "users/diary";
     }
 
     @PostMapping("/diary")
     public String saveDiaryEntries(@ModelAttribute Meal meal) {
-        Meal.save(meal);
         return "redirect:/diary";
     }
 
