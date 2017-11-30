b5cf7a2ceab38b6f1cc635c874bbf721e125bdc8
Travis Payne <thpaggie910@gmail.com>
2017-11-29 21:05:06 -0600

    Create a UserWithRoles model to handle User authentication and authorization

--- src/main/java/com/codeup/nutritrackr/models/UserWithRoles.java
+++ src/main/java/com/codeup/nutritrackr/models/UserWithRoles.java
@@ -1,4 +1,45 @@
 package com.codeup.nutritrackr.models;
 
-public class UserWithRoles {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserWithRoles extends User implements UserDetails {
+
+    public UserWithRoles(User user) {
+        super(user);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.NO_AUTHORITIES;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
+
