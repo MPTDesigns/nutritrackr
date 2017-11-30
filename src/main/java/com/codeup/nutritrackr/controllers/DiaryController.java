796c18273e3b35a6eabd2451a74e962c031aef6a
paulvaldez <valdezpaul00@gmail.com>
2017-11-30 10:30:55 -0600

    - Added a session for setting the diaryDate. - Adding methods in the diary controller for switching days using the calendar arrows.

--- src/main/java/com/codeup/nutritrackr/controllers/DiaryController.java
+++ src/main/java/com/codeup/nutritrackr/controllers/DiaryController.java
@@ -6,38 +6,48 @@ import com.codeup.nutritrackr.models.MealType;
 import com.codeup.nutritrackr.models.User;
 import com.codeup.nutritrackr.services.Meals;
 import com.codeup.nutritrackr.services.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 @Controller
-@SessionAttributes({"diaryDate"})
 public class DiaryController {
 
     private final Users users;
     private final Meals meals;
+    private final LocalDate diaryDate;
 
-    public DiaryController(Users users, Meals meals) {
+
+
+    public DiaryController(Users users, Meals meals, LocalDate diaryDate) {
         this.users = users;
         this.meals = meals;
+        this.diaryDate = diaryDate;
+
+
     }
 
     @GetMapping("/diary")
-    public String showDiaryPage(@ModelAttribute("diaryDate") LocalDate diaryDate, Model model) {
+    public String showDiaryPage(HttpSession session, Model model) {
 //        Meal mealToSave = new Meal(users.findOne(1));
 //        mealToSave.setMealType(MealType.BREAKFAST);
 //        mealToSave.setMealDate(LocalDate.now());
 //        mealToSave.setFoodItems(new ArrayList<>());
 //        mealToSave.getFoodItems().add(foodDrinks.findOne(3));
 //        meals.save(mealToSave);
-        User user = users.findOne(1);
-
+        LocalDate diaryDate = (LocalDate) session.getAttribute("diaryDate");
         if(diaryDate == null) {
             diaryDate = LocalDate.now();
+            session.setAttribute("diaryDate", diaryDate);
         }
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         Meal breakfast = meals.findMealForDayByType(user, diaryDate, MealType.BREAKFAST);
         Meal lunch = meals.findMealForDayByType(user, diaryDate, MealType.LUNCH);
@@ -61,6 +71,19 @@ public class DiaryController {
     @GetMapping("/diary/create")
     public String createDiaryPost(){
        return "users/diary";
+    }
+
+    @GetMapping("/diary/prev")
+    public String minusDate() {
+        diaryDate.minusDays(1);
+        return "redirect:/diary";
+    }
+
+    @GetMapping("/diary/next")
+    public String plusDate(@ModelAttribute LocalDate diaryDate){
+        diaryDate.plusDays(1);
+        return "redirect/diary";
+
     }
 
 
