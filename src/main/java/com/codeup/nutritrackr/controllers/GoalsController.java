ff7fd1780f0665b5a2345ca89be507b41e26f678
Travis Payne <thpaggie910@gmail.com>
2017-12-01 09:30:50 -0600

    Fix errors from the Goals section of the application

--- src/main/java/com/codeup/nutritrackr/controllers/GoalsController.java
+++ src/main/java/com/codeup/nutritrackr/controllers/GoalsController.java
@@ -37,7 +37,13 @@ public class GoalsController {
     @GetMapping("/goals/set")
     public String createUserGoals(Model model) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        model.addAttribute("goal", goals.findMostRecentUserGoals(user));
+        Goal mostRecentGoals = goals.findMostRecentUserGoals(user);
+
+        if(mostRecentGoals == null) {
+            mostRecentGoals = new Goal();
+        }
+
+        model.addAttribute("goal", mostRecentGoals);
         return "goals/set";
     }
 
