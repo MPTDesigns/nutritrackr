01a8f26561d77c18eceb3abb8f5e74a3c10820a0
Travis Payne <thpaggie910@gmail.com>
2017-12-02 12:19:28 -0600

    Update styling and routes on home page

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,59 +1,23 @@
-(function($, m) {
+(function($) {
     $(document).ready(function () {
         // navbar initializations
         $(".dropdown-button").dropdown();
         $(".button-collapse").sideNav();
-        m.udpateTextFields();
-        $('.btn.dec-btn').click(function(){
-            var inputToDecrement = $(this).siblings("input[type=number]")[0];
 
-            if(inputToDecrement.value > 0){
-                inputToDecrement.value--;
-            }
+        // Listener for diary date picker
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15, // Creates a dropdown of 15 years to control year,
+            today: 'Today',
+            clear: 'Clear',
+            close: 'Ok',
+            closeOnSelect: false // Close upon selecting a date,
         });
 
-        $(".percent.inc-btn").click(function () {
-            var inputToIncrement = $(this).siblings("input[type=number]")[0];
-
-            if(inputToIncrement.value < 100) {
-                inputToIncrement.value++;
-            }
-        });
-
-        $("#calories-inc-btn").click(function () {
-            var inputToIncrement = $(this).siblings("input[type=number]")[0];
-
-            inputToIncrement.value++;
-        });
-
-        $("input[type=number]").blur(function () {
-            var inputQty = parseInt($(this).val());
-            var max = parseInt($(this).attr("data-max"));
-
-            if(isNaN(inputQty) || inputQty < 0) {
-                $(this).val(0);
-            }
-
-            if(inputQty > max) {
-                $(this).val(max);
-            }
+        // Click listener for logout "button"
+        $("#full-logout-link, #mobile-logout-link").click(function (evt) {
+            evt.preventDefault();
+            $("#logout-form").submit();
         });
     });
-
-    $("#logout-link").click(function (evt) {
-        evt.preventDefault();
-        console.log("logout clicked");
-        $("#logout-form").submit();
-    });
-
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15, // Creates a dropdown of 15 years to control year,
-        today: 'Today',
-        clear: 'Clear',
-        close: 'Ok',
-        closeOnSelect: false // Close upon selecting a date,
-    });
-
-})(jQuery, Materialize);
-
+})(jQuery);
